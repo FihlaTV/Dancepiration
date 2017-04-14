11dbe99189537163b7f84a093a6d08fc79968223
Josien <josienjansen1@gmail.com>
2017-04-14 11:17:32 +0200

    storage fixed

--- app/src/main/java/com/example/josien/masterthesis/StartChoreo.java
+++ app/src/main/java/com/example/josien/masterthesis/StartChoreo.java
@@ -6,14 +6,20 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 
 /**
@@ -21,11 +27,79 @@ import java.util.ArrayList;
  */
 public class StartChoreo extends AppCompatActivity {
 
+    EditText t;
+    Button b;
+    String s;
+    private static final String TAG = StartChoreo.class.getName();
+    private static final String FILENAME = "myFile.txt";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.start_choreo);
+
         parseJSON();
+        };
+
+    public void dezetekst(View v) {
+        t = (EditText) findViewById(R.id.editText);
+        b = (Button) findViewById(R.id.okbutton);
+
+        s = t.getText().toString();
+
+
+        writeToFile(s  + " - ");
+
+        String textFromFileString =  readFromFile();
+
+        if (s.equals(textFromFileString) )
+            Toast.makeText(getApplicationContext(), "both string are equal", Toast.LENGTH_SHORT).show();
+        else
+            Toast.makeText(getApplicationContext(), "there is a problem", Toast.LENGTH_SHORT).show();
+    }
+
+    private void writeToFile(String data) {
+        try {
+            Log.d("Iksnaphetnietmeer", "writeToFile() returned: " + s);
+            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(openFileOutput(FILENAME, Context.MODE_APPEND));
+            outputStreamWriter.write(data);
+            outputStreamWriter.close();
+        }
+        catch (IOException e) {
+            Log.e(TAG, "File write failed: " + e.toString());
+        }
+
+    }
+
+    private String readFromFile() {
+
+        String ret = "";
+
+        try {
+            InputStream inputStream = openFileInput(FILENAME);
+
+            if ( inputStream != null ) {
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                String receiveString = "";
+                StringBuilder stringBuilder = new StringBuilder();
+
+                while ( (receiveString = bufferedReader.readLine()) != null ) {
+                    stringBuilder.append(receiveString);
+                }
+
+                inputStream.close();
+                ret = stringBuilder.toString();
+            }
+        }
+        catch (FileNotFoundException e) {
+            Log.e(TAG, "File not found: " + e.toString());
+        } catch (IOException e) {
+            Log.e(TAG, "Can not read file: " + e.toString());
+        }
+
+        Log.d("stom", "readFromFile() returned: " + ret);
+        return ret;
     }
 
     private String getJSONString(Context context)
@@ -83,5 +157,6 @@ public class StartChoreo extends AppCompatActivity {
             e.printStackTrace();
         }
     }
+
 }
 
