05ba3c26583b45aa43ecc57448a6d293bee03783
Josien <josienjansen1@gmail.com>
2017-04-18 11:34:48 +0200

    18-4

--- app/src/main/java/com/example/josien/masterthesis/MainActivity.java
+++ app/src/main/java/com/example/josien/masterthesis/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //this.initBodyParts();
     }
 
     public void nieuwechoreografie(View view){
@@ -24,5 +25,10 @@ public class MainActivity extends AppCompatActivity {
     public void bestaandechoreografie(View view){
         Toast.makeText(getApplicationContext(), "Dit kun je helaas nog niet gebruiken", Toast.LENGTH_LONG).show();
     }
+    /*
+    private void initBodyParts(){
+        new Legs() rightLeg;
+    }
+    */
 }
 
--- app/src/main/java/com/example/josien/masterthesis/StartChoreo.java
+++ app/src/main/java/com/example/josien/masterthesis/StartChoreo.java
@@ -1,11 +1,14 @@
 package com.example.josien.masterthesis;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.res.AssetManager;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -29,10 +32,11 @@ public class StartChoreo extends AppCompatActivity {
 
     String alles;
     private static final String TAG = StartChoreo.class.getName();
-    private static final String FILENAME = "myFile.txt";
+    private static final String FILENAME = "test.txt";
     private Spinner startPositie;
     private Spinner dansStijl;
     private Spinner vervolgStap;
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,8 +45,28 @@ public class StartChoreo extends AppCompatActivity {
         this.startPositie = (Spinner) this.findViewById(R.id.startpositie);
         this.dansStijl = (Spinner) this.findViewById(R.id.dansstijl);
         this.vervolgStap = (Spinner) this.findViewById(R.id.vervolgstap);
+        this.button = (Button) this.findViewById(R.id.button3);
         parseJSON();
-        };
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Creates dialog window for confirmation of logout
+                AlertDialog.Builder builder = new AlertDialog.Builder(StartChoreo.this);
+                builder
+                        .setMessage(alles)
+                        .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int id) {
+                                Toast.makeText(getApplicationContext(), "Wat vind je ervan?", Toast.LENGTH_SHORT).show();
+
+                            }
+                        })
+                        .show();
+            }
+        });
+    }
 
     public void dezetekst(View v) {
         TextView startPos  =  (TextView)startPositie.getSelectedView();
@@ -58,11 +82,9 @@ public class StartChoreo extends AppCompatActivity {
         writeToFile(alles  + " - ");
 
         String textFromFileString =  readFromFile();
+        Toast.makeText(getApplicationContext(), "Opgeslagen", Toast.LENGTH_SHORT).show();
 
-        if (alles.equals(textFromFileString) )
-            Toast.makeText(getApplicationContext(), "both string are equal", Toast.LENGTH_SHORT).show();
-        else
-            Toast.makeText(getApplicationContext(), "there is a problem", Toast.LENGTH_SHORT).show();
+        Log.d("textfromfilestring", "dezetekst() returned: " + textFromFileString);
     }
 
     private void writeToFile(String data) {
@@ -77,6 +99,7 @@ public class StartChoreo extends AppCompatActivity {
         }
 
     }
+
 
     private String readFromFile() {
 
@@ -115,7 +138,7 @@ public class StartChoreo extends AppCompatActivity {
         try
         {
             AssetManager assetManager = context.getAssets();
-            InputStream in = assetManager.open("newjson.json");
+            InputStream in = assetManager.open("1704.json");
             InputStreamReader isr = new InputStreamReader(in);
             char [] inputBuffer = new char[100];
 
@@ -155,7 +178,7 @@ public class StartChoreo extends AppCompatActivity {
                 JSONObject tijd = art_objects.getJSONObject(i);
                 String title = tijd.getString("Nummer");
                 String producer = tijd.getString("Lichaamsdeel");
-                String longtitle = tijd.getString("Duration"); //duration wordt niet meegepakt omdat de waarde leeg kan zijn (overal dus 0 invullen als het niet van toepassing is)
+                String longtitle = tijd.getString("Duration");
                 String productionplace = tijd.getString("Style");
                 Log.d("zalhetdan", "parseJSON() returned: " + title + producer + longtitle + productionplace);
             }
@@ -165,5 +188,8 @@ public class StartChoreo extends AppCompatActivity {
         }
     }
 
+    public void veranderchoreo(View view){
+        Toast.makeText(getApplicationContext(), "Dit kun je helaas nog niet gebruiken", Toast.LENGTH_SHORT).show();
+    }
 }
 
