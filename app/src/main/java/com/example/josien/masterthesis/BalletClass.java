aec0d0c96d18ed3259186ddf5b305bdba01b4b54
Josien <josienjansen1@gmail.com>
2017-04-21 17:30:30 +0200

    RANDOMIZER WORKS

--- app/src/main/java/com/example/josien/masterthesis/BalletClass.java
+++ app/src/main/java/com/example/josien/masterthesis/BalletClass.java
@@ -1,12 +1,17 @@
 package com.example.josien.masterthesis;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.res.AssetManager;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -19,6 +24,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.ArrayList;
+import java.util.Random;
 
 /**
  * Created by Josien on 21-4-2017.
@@ -29,7 +35,16 @@ public class BalletClass extends AppCompatActivity{
     private static final String FILENAME = "b.txt";
     String dancestyle;
     ArrayList<String> responseList = new ArrayList<>();
-    String newline = "\r\n";
+    String bestaandechoreo;
+    String nieuwesuggestie;
+    Spinner textView;
+    Spinner textView2;
+    Spinner textView3;
+    Spinner textView4;
+    Spinner textView5;
+    JSONArray art_objects;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -71,7 +86,6 @@ public class BalletClass extends AppCompatActivity{
 
                 inputStream.close();
                 ret = stringBuilder.toString();
-                Log.d("JAJAJAJA", "readFromFile() returned: " + ret);
             }
         }
         catch (FileNotFoundException e) {
@@ -79,8 +93,6 @@ public class BalletClass extends AppCompatActivity{
         } catch (IOException e) {
             Log.e(TAG, "Can not read file: " + e.toString());
         }
-
-        Log.d("stom", "readFromFile() returned: " + ret);
         return ret;
     }
 
@@ -124,7 +136,7 @@ public class BalletClass extends AppCompatActivity{
 
 
         try {
-            JSONArray art_objects = json.getJSONArray("record");
+            art_objects = json.getJSONArray("record");
 
             // Loop through the items till it ends
             for (int i = 0; i < art_objects.length(); i++) {
@@ -133,48 +145,45 @@ public class BalletClass extends AppCompatActivity{
                 String pasnaam = tijd.getString("Pasnaam");
                 dancestyle = tijd.getString("Style");
                 String beschrijving = tijd.getString("Beschrijving");
-                Log.d("zalhetdan", "parseJSON() returned: " + nummer + " " + pasnaam +  " " +dancestyle + " " + beschrijving);
                 if (dancestyle.equals("Ballet")) {
                     writeToFile(beschrijving);
 
                     responseList.add(beschrijving);
                     String textFromFileString = readFromFile();
-                    Log.d("watstaathier", "parseJSON() returned: " + textFromFileString);
-                    Log.d("watkomthieruit", "parseJSON() returned: " + responseList);
 
                     ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
                             android.R.layout.simple_dropdown_item_1line, responseList);
-                    Spinner textView = (Spinner)
+                    textView = (Spinner)
                             findViewById(R.id.autocomplete);
                     textView.setAdapter(adapter);
 
                     ArrayAdapter<String> adapter2 = new ArrayAdapter<>(this,
                             android.R.layout.simple_dropdown_item_1line, responseList);
-                    Spinner textView2 = (Spinner)
+                    textView2 = (Spinner)
                             findViewById(R.id.autocomplete2);
                     textView2.setAdapter(adapter2);
 
                     ArrayAdapter<String> adapter3 = new ArrayAdapter<>(this,
                             android.R.layout.simple_dropdown_item_1line, responseList);
-                    Spinner textView3 = (Spinner)
+                    textView3 = (Spinner)
                             findViewById(R.id.autocomplete3);
                     textView3.setAdapter(adapter3);
 
                     ArrayAdapter<String> adapter4 = new ArrayAdapter<>(this,
                             android.R.layout.simple_dropdown_item_1line, responseList);
-                    Spinner textView4 = (Spinner)
+                    textView4 = (Spinner)
                             findViewById(R.id.autocomplete4);
                     textView4.setAdapter(adapter4);
 
                     ArrayAdapter<String> adapter5 = new ArrayAdapter<>(this,
                             android.R.layout.simple_dropdown_item_1line, responseList);
-                    Spinner textView5 = (Spinner)
+                    textView5 = (Spinner)
                             findViewById(R.id.autocomplete5);
                     textView5.setAdapter(adapter5);
+
                 }
 
             }
-
         }
         catch (JSONException e){
             e.printStackTrace();
@@ -184,4 +193,62 @@ public class BalletClass extends AppCompatActivity{
     // get balletsteps
     // parse it
     // send it to existing steps
+
+    public void okey (View view) throws JSONException {
+
+        TextView stap1  =  (TextView)textView.getSelectedView();
+        TextView stap2  =  (TextView)textView2.getSelectedView();
+        TextView stap3  =  (TextView)textView3.getSelectedView();
+        TextView stap4  =  (TextView)textView4.getSelectedView();
+        TextView stap5  =  (TextView)textView5.getSelectedView();
+        String Stap1 = stap1.getText().toString();
+        String Stap2 = stap2.getText().toString();
+        String Stap3 = stap3.getText().toString();
+        String Stap4 = stap4.getText().toString();
+        String Stap5 = stap5.getText().toString();
+
+        ArrayList<String> List = new ArrayList<>();
+        List.add(Stap1);
+        List.add(Stap2);
+        List.add(Stap3);
+        List.add(Stap4);
+        List.add(Stap5);
+
+        bestaandechoreo = Stap1 + "\r\n" + Stap2 + "\r\n" + Stap3 + "\r\n" + Stap4 + "\r\n" + Stap5;
+
+        Random random = new Random();
+        String R = List.get(random.nextInt(List.size()));
+        Log.d("Paktierandom?", "okey() returned: " + R);
+        String pas1 = responseList.get(random.nextInt(responseList.size()));
+        Log.d("Randompas?", "okey() returned: " + pas1);
+        if (List.contains(pas1)){
+            Log.d("zitalindelijst", "okey() returned: " + pas1 + R);
+            pas1= responseList.get(random.nextInt(responseList.size()));
+        }
+        if (R.equals(pas1)){
+            Log.d("dezelfdepassen", "okey() returned: " + pas1 + R);
+            pas1 = responseList.get(random.nextInt(responseList.size()));
+        }
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(BalletClass.this);
+            builder
+                    .setMessage("Dit is nu je choreografie: \r\n" + bestaandechoreo +
+                            "\r\nverander " + R + " naar: " + pas1)
+                    .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int id) {
+
+                        }
+                    })
+                    .show();
+        }
+
+
+        //R = R.replace(R, pas1);
+
+
+    public void algorithm (View view){
+        //do something really smart
+    }
+
 }
